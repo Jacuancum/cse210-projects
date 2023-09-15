169821f3560a8bfb6e8e191ec09e8c9c06dc343f
Jacuancum <cks2306@gmail.com>
2023-09-15 15:07:01 +0800

    C# prep 1

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello SB World!!!");
+        Console.WriteLine("This is in C#.");
+
+        Console.Write("What color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine("Your color is " + color + ".");
+        Console.WriteLine($"Your color is {color}.");
     }
 }
