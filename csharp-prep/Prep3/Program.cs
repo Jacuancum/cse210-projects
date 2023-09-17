36f1c4f52a653c28e757ebd53c9b089585b08b6c
Jacuancum <cks2306@gmail.com>
2023-09-17 14:03:04 +0800

    prep3 core1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        string number;
+        int answer;
+        Console.Write("What is the magic number? ");
+        number = Console.ReadLine();
+        answer = Convert.ToInt32(number);
+
+
+        string guess;
+        int guessnumber;
+        
+        Console.Write("What is your guess?");
+        guess = Console.ReadLine();
+        guessnumber = Convert.ToInt32(guess);
+        
+        if (guessnumber > answer)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessnumber < answer)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
