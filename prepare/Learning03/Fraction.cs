f72ab0b06f7cd99c835fa037b5a14379aba99bcf
Jacuancum <cks2306@gmail.com>
2023-10-12 21:57:13 +0800

    cse210_05_fraction

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,41 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        // Default to 1/1
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _numerator = top;
+        _denominator = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is is just a temporary, local variable that will be recomputed each time this is called.
+        string text = $"{_numerator}/{_denominator}";
+        return text;
+    }
+
+    public double GetDecimalValue()
+    {
+        // Notice that this is not stored as a member variable.
+        // Is will be recomputed each time this is called.
+        return (double)_numerator / (double)_denominator;
+    }
+}
