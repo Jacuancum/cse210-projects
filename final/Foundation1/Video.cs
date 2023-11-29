5cd4aa5831c34c1d3a317d8d8f02c3fb3d86dcb7
Jacuancum <cks2306@gmail.com>
2023-11-29 10:48:14 +0800

    cse210_final 1 update

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -1,12 +1,32 @@
+using System.Diagnostics.Contracts;
+
 class Video
 {
     public string _title;
+    public string _author;
+    public int _length;
+    private List<Comment> comments;
     public string Title()
     {
         return _title;
     }
-    public Video(string title)
+    public string Author()
+    {
+        return _author;
+    }
+    public int Length()
+    {
+        return _length;
+    }
+    public List<Comment> Comments()
+    {
+        return comments;
+    }
+    public Video(string title, string author, int length)
     {
         _title = title;
+        _author = author;
+        _length = length;
+        comments = new List<Comment>();
     }
 }
